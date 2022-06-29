36744499d1cab63aac70e441e26d51bd35e66096
training-octobit8 <97100124+training-octobit8@users.noreply.github.com>
2022-06-29 19:55:21 +0530

    Update App.java

--- app/src/main/java/gradleApp/App.java
+++ app/src/main/java/gradleApp/App.java
@@ -5,7 +5,7 @@ package gradleApp;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello DevOps world !";
     }
 
     public static void main(String[] args) {
