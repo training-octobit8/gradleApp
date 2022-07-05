a82305d29849f5934ee44419a2c22bb0ba38c487
training-octobit8 <97100124+training-octobit8@users.noreply.github.com>
2022-07-05 20:52:03 +0530

    Update App.java

--- app/src/main/java/gradleApp/App.java
+++ app/src/main/java/gradleApp/App.java
@@ -5,7 +5,7 @@ package gradleApp;
 
 public class App {
     public String getGreeting() {
-        return "Hello Jenkins DevOps World!";
+        return "Hello Jenkins from GitHub!";
     }
 
     public static void main(String[] args) {
